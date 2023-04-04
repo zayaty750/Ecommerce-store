f0e5d1eca1f8736f03b709546e1cba044c1cb9fa
zeyad <zeyad2103469@miuegypt.edu.eg>
2023-04-05 01:25:36 +0200

    by zayaty about_us page, contact_us in the home page,new top and bottom navbar

--- script.js
+++ script.js
@@ -7,82 +7,14 @@ window.addEventListener("load", function(){
 
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "20%";
-    document.getElementById("main").style.marginRight = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("mySidenav").style.display = "block";
+    document.getElementById("open_top_nav_bar").style.display = "none";
   }
   
   function closeNav() {
-    document.getElementById("mySidenav").style.width = "0%";
-    document.body.style.backgroundColor = "white";
+    document.getElementById("mySidenav").style.display = "none";
+    document.getElementById("open_top_nav_bar").style.display = "block";
   }
-function tes()
-{
-
-  document.getElementById("video1").style.width = "100%";
-}
-window.onscroll = function() {change_style_when_scroll()};
-function change_style_when_scroll() {
-
-
-  if (document.documentElement.scrollTop > 550) 
-  {
-        
-        document.getElementById("container").style.opacity = "1";  
-        document.getElementById("container").style.translate = "0px"; 
-
-        document.getElementById("container1").style.opacity = "1"; 
-        document.getElementById("container1").style.translate = "0px"; 
-
-        document.getElementById("container2").style.opacity = "1";  
-        document.getElementById("container2").style.translate = "0px";  
-        
-        document.getElementById("container3").style.opacity = "1"; 
-        document.getElementById("container3").style.translate = "0px"; 
-        
-        document.getElementById("container4").style.opacity = "1";  
-        document.getElementById("container4").style.translate = "0px";  
-          
-
-        document.getElementById("change_color_upper_nav").className = "change_color_upper_navtoblack";
-        document.getElementById("change_color_upper_nav1").className = "change_color_upper_navtoblack";
-        document.getElementById("change_color_upper_nav2").className = "change_color_upper_navtoblack";
-        document.getElementById("change_color_upper_nav4").className = "change_color_upper_navtoblack";
-    }
-    else 
-    {
-      document.getElementById("container").style.opacity = "0";  
-      document.getElementById("container").style.translate = "-1000px"; 
-
-      document.getElementById("container1").style.opacity = "0"; 
-      document.getElementById("container1").style.translate = "-1000px"; 
-
-      document.getElementById("container2").style.opacity = "0";  
-      document.getElementById("container2").style.translate = "-1000px"; 
-      
-      document.getElementById("container3").style.opacity = "0"; 
-      document.getElementById("container3").style.translate = "-1000px";
-      
-      document.getElementById("container4").style.opacity = "0";  
-      document.getElementById("container4").style.translate = "-1000px";  
-
-        document.getElementById("change_color_upper_nav").className = "change_color_upper_navtowhite";
-        document.getElementById("change_color_upper_nav1").className = "change_color_upper_navtowhite";
-        document.getElementById("change_color_upper_nav2").className = "change_color_upper_navtowhite";
-        document.getElementById("change_color_upper_nav4").className = "change_color_upper_navtowhite";
-    }
-    if(document.documentElement.scrollTop > 1000)
-    {
-      document.getElementById("change_color_upper_nav").className = "change_color_upper_navtoblack";
-      document.getElementById("change_color_upper_nav1").className = "change_color_upper_navtoblack";
-      document.getElementById("change_color_upper_nav2").className = "change_color_upper_navtoblack";
-      document.getElementById("change_color_upper_nav4").className = "change_color_upper_navtoblack";
-    }
-    else
-    {
-
-    }
-}
 
 let myindex = 0;
 function showSlides() 
@@ -103,48 +35,19 @@ setTimeout(showSlides, 1000);
 function open_search()
 {
   document.getElementById("search_nav").style.height = "100%";
-  document.getElementById("change_color_upper_nav").className = "change_color_upper_navtoblack";
-  document.getElementById("change_color_upper_nav1").className = "change_color_upper_navtoblack";
-  document.getElementById("change_color_upper_nav2").className = "change_color_upper_navtoblack";
-  document.getElementById("change_color_upper_nav4").className = "change_color_upper_navtoblack";
-  document.body.style.overflow= "hidden";
-  document.getElementById("change_color_upper_nav").style.display = "none";
-  document.getElementById("change_color_upper_nav1").style.display = "none";
-  document.getElementById("change_color_upper_nav2").style.display = "none";
+  document.body.style.overflow = "hidden";
+
 }
 function close_search()
 {
   document.getElementById("search_nav").style.height = "0px";
-  document.getElementById("change_color_upper_nav").className = "change_color_upper_navtowhite";
-  document.getElementById("change_color_upper_nav1").className = "change_color_upper_navtowhite";
-  document.getElementById("change_color_upper_nav2").className = "change_color_upper_navtowhite";
-  document.getElementById("change_color_upper_nav4").className = "change_color_upper_navtowhite";
-  document.body.style.overflowY= "auto";
-  document.getElementById("change_color_upper_nav").style.display = "block";
-  document.getElementById("change_color_upper_nav1").style.display = "block";
-  document.getElementById("change_color_upper_nav2").style.display = "block";
+  document.body.style.overflowY = "visible";
 }
+
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-function load()
-{
-  document.getElementById("word").style.transition = "3s ease-in-out";
-  document.getElementById("word").style.transform = "rotate(360deg)";
-  document.getElementById("word").style.opacity = "1";
-
-  document.getElementById("word2").style.transitionDelay = "4s";
-  document.getElementById("word2").style.transition = "3s ease-in-out";
-  document.getElementById("word2").style.transform = "rotate(360deg)";
-  document.getElementById("word2").style.opacity = "1";
-
-  document.getElementById("word3").style.transitionDelay = "5s";
-  document.getElementById("word3").style.transition = "4s ease-in-out";
-  document.getElementById("word3").style.transform = "rotate(360deg)";
-  document.getElementById("word3").style.opacity = "1";
-}
-
 
 //Date script.
 const yearSelect = document.getElementById("year");
